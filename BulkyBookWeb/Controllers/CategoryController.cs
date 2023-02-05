b1df47aad3749782e6d40121fc305e8e3daccbbe
Derek Hanson <100658364+derekhanson1996@users.noreply.github.com>
2023-02-05 17:37:24 -0600

    Added category tab with read functionality as well as some styling changes

---
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -0,0 +1,23 @@
+﻿using BulkyBookWeb.Data;
+using BulkyBookWeb.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Controllers
+{
+    public class CategoryController : Controller
+    {
+        private readonly ApplicationDbContext _dp;
+
+        public CategoryController(ApplicationDbContext dp)
+        {
+            _dp = dp;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objCategoryList = _dp.Categories;  //Gets all records from Category table
+
+            return View(objCategoryList);
+        }
+    }
+}
