326b6b966e5b2f241f130ed7c4c51ab200c40d90
Derek Hanson <100658364+derekhanson1996@users.noreply.github.com>
2023-02-05 20:49:35 -0600

    Added category create/edit/delete functionality and alerts

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -6,18 +6,116 @@ namespace BulkyBookWeb.Controllers
 {
     public class CategoryController : Controller
     {
-        private readonly ApplicationDbContext _dp;
+        private readonly ApplicationDbContext _db;
 
-        public CategoryController(ApplicationDbContext dp)
+        public CategoryController(ApplicationDbContext db)
         {
-            _dp = dp;
+            _db = db;
         }
 
         public IActionResult Index()
         {
-            IEnumerable<Category> objCategoryList = _dp.Categories;  //Gets all records from Category table
+            IEnumerable<Category> objCategoryList = _db.Categories;  //Gets all records from Category table
 
             return View(objCategoryList);
+        }
+
+        //GET
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Category obj)
+        {
+            if(obj.Name == obj.DisplayOrder.ToString())     //Custom validation on server side
+            {
+                ModelState.AddModelError("name", "The Display Order cannot match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Add(obj);    //Adds Category to table if valid
+                _db.SaveChanges();
+                TempData["success"] = "Category created successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Edit(int? id)
+        {
+            if (id == null || id ==0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find(id);   //Finds Category with that id
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Category obj)
+        {
+            if (obj.Name == obj.DisplayOrder.ToString())     //Custom validation on server side
+            {
+                ModelState.AddModelError("name", "The Display Order cannot match the Name.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                _db.Categories.Update(obj);    //Updates Category on table if valid
+                _db.SaveChanges();
+                TempData["success"] = "Category updated successfully";
+
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var categoryFromDb = _db.Categories.Find(id);   //Finds Category with that id
+
+            if (categoryFromDb == null)
+            {
+                return NotFound();
+            }
+
+            return View(categoryFromDb);
+        }
+
+        //POST
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Category obj)
+        {
+            _db.Categories.Remove(obj);    //Deletes Category on table if valid
+            _db.SaveChanges();
+            TempData["success"] = "Category deleted successfully";
+
+            return RedirectToAction("Index");
         }
     }
 }
